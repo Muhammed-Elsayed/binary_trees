7d2faa6be520cf883bb2dc0ea23b08ef34587e38
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-08-30 20:05:42 +0300

    9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -4,16 +4,17 @@
 /**
  *binary_tree_height - calculate the height of tree.
  * @tree: ptr to the root node.
- *return: height of the tree.
+ *Return: height of the tree.
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left, right, height;
+
 	if (tree == NULL)
 		return (-1);
 
 	left = 1 + binary_tree_height(tree->left);
-	right = 1 + binary_tree_height(tree->right); 
+	right = 1 + binary_tree_height(tree->right);
 	if (left > right)
 		height = left;
 
