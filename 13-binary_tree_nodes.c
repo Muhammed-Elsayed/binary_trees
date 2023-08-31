b0d982bbdd9362155160788ed709bf54f188f373
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-08-31 23:33:17 +0300

    13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -15,9 +15,10 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left != NULL || tree->right != NULL)
+	{
 		count++;
-
-	count += binary_tree_nodes(tree->left);
-	count += binary_tree_nodes(tree->right);
+		count += binary_tree_nodes(tree->left);
+		count += binary_tree_nodes(tree->right);
+	}
 	return (count);
 }
